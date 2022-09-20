ce343407aa03039f73bc0e383aa2ce882632e196
Anqi-Tech <anqihu1107@gmail.com>
2022-09-20 12:27:58 -0400

    the dice points work

--- project/src/components/FirstComp.vue
+++ project/src/components/FirstComp.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
+      <div class="game-field">
+        <div>
+          <h1>Hi: {{playerHealth}}</h1>
+        </div>
+        <div>
+          <h1 @click="selectSkill1">Deal [number] x 10 damage</h1>
+          <h1 @click="selectSkill2">Heal [number] x 5 health</h1>
+        </div>
+        <div>
+          <h1>Bye: {{enemyHealth}}</h1>
+        </div>
+      </div>
+      <div class="game-dice">
         <div v-for="(die, index) in dice" :key="die.id" class="dice" @click="selectDice">
             <p>{{die.number}}</p>
             <p class="hide">{{index}}</p>
         </div>
-        <h1 @click="selectSkill1">Deal [number] x 10 damage</h1>
-        <h1 @click="selectSkill2">Heal [number] x 5 health</h1>
+      </div>
     </div>
 </template>
 
@@ -14,24 +26,50 @@ export default {
    data() {
     return {
       dice: [],
+      targetedDice: null,
+      diceNumber: null,
+      indexNumber: null,
       diceSelected: false,
       skillSelected: false,
-      diceNumber: null,
       damageNumber: null,
       healNumber: null,
+      playerHealth: 100,
+      enemyHealth: 1000,
     }
   },
   methods: {
     selectDice(e) {
-        const targetDice = e.target
-        this.diceNumber = targetDice.innerText
+      if (e.target.tagName == 'DIV') {
+        this.targetedDice = e.target
+      } else {
+        this.targetedDice = e.target.parentElement
+      }
+        this.diceNumber = this.targetedDice.innerText
+        this.indexNumber = this.targetedDice.lastElementChild.innerHTML
         this.diceSelected = true
-        console.log(targetDice.lastElementChild.innerHTML)
     },
     selectSkill1() {
       if (this.diceSelected == true) {
+        const usedDice = this.dice[this.indexNumber]
+        this.dice = this.dice.filter((d) => d !== usedDice)
         this.damageNumber = this.diceNumber * 10
-        console.log(this.damageNumber)
+        this.enemyHealth = this.enemyHealth - this.damageNumber
+      }
+    },
+    selectSkill2() {
+      if (this.diceSelected == true) {
+        const usedDice = this.dice[this.indexNumber]
+        this.dice = this.dice.filter((d) => d !== usedDice)
+        this.healNumber = this.diceNumber * 5
+        this.playerHealth = this.playerHealth + this.healNumber
+      }
+    },
+    reloadDice() {
+      if (this.dice.length === 0) {
+        for (let i = 0; i < 4; i++) {
+          let randomNumber = Math.floor(Math.random()*6)+1
+          this.dice.push({number: randomNumber})
+        }        
       }
     }
   },
@@ -40,11 +78,27 @@ export default {
     let randomNumber = Math.floor(Math.random()*6)+1
     this.dice.push({number: randomNumber})
     }
+  },
+  updated() {
+    if (this.dice.length == 0) {
+      for (let i = 0; i < 4; i++) {
+        let randomNumber = Math.floor(Math.random()*6)+1
+        this.dice.push({number: randomNumber})
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
+.game-field {
+  display: flex;
+}
+
+.game-dice {
+  display: flex;
+}
+
 .dice {
     background: red;
     width: 5vw;
